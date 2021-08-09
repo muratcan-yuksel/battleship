00f6e6a6bfcfe1d9a7b56258b1157b0ac32b3cf9
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-09 08:10:18 +0300

    fixed the problems with the factory function. It was fauly back then

--- src/ship.js
+++ src/ship.js
@@ -1,12 +1,21 @@
 //create a ship factory function
-const Ship= (length, coordinate)=>{
+const Ship= (length, coordinate, number)=>{
+  const getLength= () => length;
+  const getCoordinate= ()=> coordinate;
     let position=[];
     for( let i= coordinate; i< (coordinate + length); i++){
 position.push(i)
     }
+   const hit= (number)=>{
+if (position.indexOf(number) > -1){
+    return "hit!"
+}else {
+    return "miss!"
+}
+    }
 
     
-return(position)    
+return{position,getLength, getCoordinate,hit}    
 
 }
 
