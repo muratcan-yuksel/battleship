3a9252f68e7128d67e724b3f105778fd8ac1377a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-13 17:47:23 +0300

    wrote 2 more tests

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -16,6 +16,8 @@ const gameBoard = () => {
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   ];
 
+  //   const receiveAttack = (coordinates) => {};
+
   return { board };
 };
 
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -25,3 +25,35 @@ test("can I give coordinates while calling the ship factory?", () => {
   player.ship1.coordinates = { horizontal: 3, vertical: 2 };
   expect(player.ship1.coordinates).toEqual({ horizontal: 3, vertical: 2 });
 });
+
+test("when a ship receives attack ", () => {
+  const player = gameBoard();
+  player.ship1 = Ship(3);
+  player.ship1.coordinates = { vertical: 2, horizontal: 3 };
+  let coordinatesX = [2, 3];
+  player.receiveAttack = (coordinates) => {
+    if (
+      coordinatesX[0] == player.ship1.coordinates.vertical &&
+      coordinatesX[1] == player.ship1.coordinates.horizontal
+    ) {
+      return "hits!";
+    } else return "of";
+  };
+  expect(player.receiveAttack()).toEqual("hits!");
+});
+
+test("when a ship avoids attack", () => {
+  const player = gameBoard();
+  player.ship1 = Ship(3);
+  player.ship1.coordinates = { vertical: 2, horizontal: 3 };
+  let coordinatesX = [2, 6];
+  player.receiveAttack = (coordinates) => {
+    if (
+      coordinatesX[0] == player.ship1.coordinates.vertical &&
+      coordinatesX[1] == player.ship1.coordinates.horizontal
+    ) {
+      return "hits!";
+    } else return "of";
+  };
+  expect(player.receiveAttack()).toEqual("of");
+});
