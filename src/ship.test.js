5bb45a3c2cabb962dd1b553477b9fb2f0cfe74bc
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-17 13:34:33 +0300

    added a test that shows the last state of the array

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,7 +1,12 @@
 const Ship = require("./ship");
 
-test("does array takes hit functions ", () => {
+test("does array takes hit functions (true version) ", () => {
   const bateau = Ship(4);
   bateau.hit(1);
   expect(bateau.shipParts).toEqual([0, "hit", 2, 3]);
+});
+xtest("does array takes hit functions 2", () => {
+  const bateau = Ship(4);
+  bateau.hit(1);
+  expect(bateau.shipParts).toEqual([0, "hit", "hit", 3]);
 });
